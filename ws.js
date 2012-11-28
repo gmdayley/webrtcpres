fa87288bb8e89a1c7d543313ba131c85f6129c72
Gabriel Dayley <gabriel.dayley@gmail.com>
2012-11-28 09:27:11 -0700

    initial commit

---
+++ ws.js
@@ -0,0 +1,23 @@
+var express = require('express')
+//    app = require('express')()
+    , app = express()
+    , server = require('http').createServer(app)
+    , io = require('socket.io').listen(server);
+
+server.listen(3006);
+
+app.use("/", express.static(__dirname));
+
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('connection', {});
+
+    socket.on('message', function(msg){
+        console.log('message', msg);
+        socket.broadcast.emit('msg', msg);
+    });
+
+    socket.on('remote value', function(data) {
+        socket.broadcast.emit('remote', data);
+    });
+});
